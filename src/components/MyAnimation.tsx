a17bfc2a52504d91f27c4e4bd74ab464a06d8015
Sadman600 <sadmansakib600@gmail.com>
2024-01-30 17:32:36 +0600

    add animation

--- src/components/MyAnimation.tsx
+++ src/components/MyAnimation.tsx
@@ -1,10 +1,23 @@
+import { motion } from "framer-motion";
 import "./MyAnimation.css";
+
+const parent = {
+  initial: { rotate: 0 },
+  animate: { rotate: 360 },
+};
+
 const MyAnimation = () => {
   return (
     <>
-      <div className="animationContainer">
+      <motion.div
+        className="animationContainer"
+        variants={parent}
+        initial="initial"
+        animate="animate"
+        transition={{ duration: 2 }}
+      >
         <h1>Hell Animate</h1>
-      </div>
+      </motion.div>
     </>
   );
 };
